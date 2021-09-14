c462b048a3c81060d1c3c62c9ca3b9fbc307facd
Josue Vazquez Rojas <josuealfredovrojas@outlook.com>
2021-09-13 21:50:01 -0500

    Logout
    Se agrega la opción de poder cerrar la sesión del usuario.

--- resources/views/layouts/mainDash.blade.php
+++ resources/views/layouts/mainDash.blade.php
@@ -79,15 +79,18 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">¿Listo para salir?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-body">Al dar en salir su sesión se cerarra.</div>
                 <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button class="btn btn-primary" type="submit">Salir</button>
+                    </form>
                 </div>
             </div>
         </div>
