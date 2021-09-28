edeaa1d5d043261e7b8abac3a0d3c8414df06c13
Josue Vazquez Rojas <josuealfredovrojas@outlook.com>
2021-09-28 17:28:59 -0500

    Sidebar - Cambio de la opción repartidor
    Se cambia la opción del repartidor por la de personal.

--- resources/views/layouts/sidebarDash.blade.php
+++ resources/views/layouts/sidebarDash.blade.php
@@ -81,13 +81,13 @@
                         <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseRepartidor"
                     aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-motorcycle"></i>
-                    <span>Repartidores</span>
+                    <i class="fas fa-users"></i>
+                    <span>Personal</span>
                 </a>
                 <div id="collapseRepartidor" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Utilidades de repartidor:</h6>
+                        <h6 class="collapse-header">Utilidades del personal:</h6>
                         <a class="collapse-item" href="#">Agregar</a>
                         <a class="collapse-item" href="#">Buscar</a>
                     </div>
